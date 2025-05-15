9f135a409341ada5871d839186aa6542a9617785
Amine <affi.amin.work@gmail.com>
2025-05-15 15:52:00 +0100

    Refactor OCP exercise

--- OCP/src/com/directi/training/ocp/exercise/ResourceAllocator.java
+++ OCP/src/com/directi/training/ocp/exercise/ResourceAllocator.java
@@ -1,67 +1,32 @@
 package com.directi.training.ocp.exercise;
 
-public class ResourceAllocator
-{
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceAllocator {
     private static final int INVALID_RESOURCE_ID = -1;
+    private final Map<String, Resource> resourceMap = new HashMap<>();
 
-    public int allocate(ResourceType resourceType)
-    {
-        int resourceId;
-        switch (resourceType) {
-        case TIME_SLOT:
-            resourceId = findFreeTimeSlot();
-            markTimeSlotBusy(resourceId);
-            break;
-        case SPACE_SLOT:
-            resourceId = findFreeSpaceSlot();
-            markSpaceSlotBusy(resourceId);
-            break;
-        default:
+    public ResourceAllocator() {
+        resourceMap.put("TIME_SLOT", new TimeSlotResource());
+        resourceMap.put("SPACE_SLOT", new SpaceSlotResource());
+    }
+
+    public int allocate(String resourceType) {
+        Resource resource = resourceMap.get(resourceType);
+        if (resource == null) {
             System.out.println("ERROR: Attempted to allocate invalid resource");
-            resourceId = INVALID_RESOURCE_ID;
-            break;
+            return INVALID_RESOURCE_ID;
         }
-        return resourceId;
+        return resource.allocate();
     }
 
-    public void free(ResourceType resourceType, int resourceId)
-    {
-        switch (resourceType) {
-        case TIME_SLOT:
-            markTimeSlotFree(resourceId);
-            break;
-        case SPACE_SLOT:
-            markSpaceSlotFree(resourceId);
-            break;
-        default:
+    public void free(String resourceType, int resourceId) {
+        Resource resource = resourceMap.get(resourceType);
+        if (resource == null) {
             System.out.println("ERROR: attempted to free invalid resource");
-            break;
+            return;
         }
-    }
-
-    private void markSpaceSlotFree(int resourceId)
-    {
-    }
-
-    private void markTimeSlotFree(int resourceId)
-    {
-    }
-
-    private void markSpaceSlotBusy(int resourceId)
-    {
-    }
-
-    private int findFreeSpaceSlot()
-    {
-        return 0;
-    }
-
-    private void markTimeSlotBusy(int resourceId)
-    {
-    }
-
-    private int findFreeTimeSlot()
-    {
-        return 0;
+        resource.free(resourceId);
     }
 }
